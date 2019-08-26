ebd2c78e86b6cab02a78b85de05ae23985e23248
surgeNexus <jgwebb@outlook.com>
2019-08-26 17:31:44 -0400

    add google membership form

--- routes/info.js
+++ routes/info.js
@@ -7,7 +7,9 @@ var Campground = require("../models/campground");
 var Comment = require("../models/comment")
 var middleware = require("../middleware");
 
-
+router.get("/join", function(req, res){
+    res.render("info/join");
+})
 
 // router.get('/newsletter', function (req, res) {
 //     var filePath = "./public/files/pdf/august.pdf";
