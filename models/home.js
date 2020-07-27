5cdc7c857a405d4cb7d4c741cd51073aa3fdda4a
surgeNexus <jgwebb@outlook.com>
2020-07-27 17:40:33 -0400

    update mailing list form to info email address

--- models/home.js
+++ models/home.js
@@ -1,22 +1,23 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
 var homeSchema = mongoose.Schema({
-    title: String,
-    body: String,
-    link1: String,
-    link1name: String,
-    link2: String,
-    link2name: String,
-    image1: String,
-    image2: String,
-    image3: String,
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        },
-        username: String
-    }
+  title: String,
+  body: String,
+  link1: String,
+  link1name: String,
+  link2: String,
+  link2name: String,
+  image1: String,
+  image2: String,
+  image3: String,
+  order: Number,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    username: String
+  }
 });
 
-module.exports = mongoose.model("Home", homeSchema);
+module.exports = mongoose.model('Home', homeSchema);
