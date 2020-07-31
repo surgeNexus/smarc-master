0f23a2f16cc9b65e9efea98e787e62388511e5d1
surgeNexus <jgwebb@outlook.com>
2020-07-30 22:15:14 -0400

    sort users by last name

--- routes/member.js
+++ routes/member.js
@@ -12,7 +12,7 @@ router.get('/', middleware.isLoggedIn, function (req, res) {
       console.log(err);
     } else {
       if (foundUser.isMember === true) {
-        User.find({}, function (err, foundUsers) {
+        User.find({}, null, {sort: {lastName: -1}}, function (err, foundUsers) {
           res.render('members/index', { members: foundUsers });
         });
       } else {
