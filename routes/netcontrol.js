0ec63b834c5d0cbfcc62265a3d24b812f69f13fe
surgeNexus <jgwebb@outlook.com>
2020-08-06 14:43:24 -0400

    add members only to NCS

--- routes/netcontrol.js
+++ routes/netcontrol.js
@@ -7,7 +7,7 @@ var moment = require('moment');
 const { update } = require('../models/user');
 const user = require('../models/user');
 
-router.get('/', function (req, res) {
+router.get('/', middleware.isMember, function (req, res) {
   Net.find({})
     .populate('ncs')
     .sort({ date: 1 })
