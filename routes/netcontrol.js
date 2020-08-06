199148657599e3e66f55efba7c9a25375078862d
surgeNexus <jgwebb@outlook.com>
2020-08-06 15:12:00 -0400

    to upper on members

--- routes/netcontrol.js
+++ routes/netcontrol.js
@@ -7,7 +7,7 @@ var moment = require('moment');
 const { update } = require('../models/user');
 const user = require('../models/user');
 
-router.get('/', middleware.isMember, function (req, res) {
+router.get('/', function (req, res) {
   Net.find({})
     .populate('ncs')
     .sort({ date: 1 })
