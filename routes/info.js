957743cbd94786959d13ffad13cfa84cb4e68f7a
Jordan Webb <jordan@decad3.com>
2021-01-25 14:08:37 -0500

    member documents section

--- routes/info.js
+++ routes/info.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 var fs = require('fs');
 var User = require('../models/user');
 var Campground = require('../models/campground');
-var Comment = require('../models/comment');
+var Docs = require('../models/document');
 var middleware = require('../middleware');
 
 router.get('/join', function (req, res) {
@@ -22,6 +22,93 @@ router.get('/join', function (req, res) {
 
 router.get('/email', function (req, res) {
   res.render('info/email');
+});
+
+router.get('/docs', function (req, res) {
+  Docs.find({}, function (err, foundDocs) {
+    if (err || !foundDocs) {
+      req.flash('error', 'Item not found');
+      res.redirect('back');
+    } else {
+      res.render('info/docs', { foundDocs: foundDocs });
+    }
+  });
+});
+
+router.get('/docs/new', middleware.isAdmin, function (req, res) {
+  Docs.find({}, function (err, newSk) {
+    if (err) {
+      req.flash('error', 'Something went wrong');
+      res.redirect('back');
+    } else {
+      res.render('info/docs/new', { newSk: newSk });
+    }
+  });
+});
+
+router.post('/docs', middleware.isAdmin, function (req, res) {
+  var title = req.body.title;
+  var date = req.body.date;
+
+  let doc = req.files.doc;
+  doc.mv('./public/files/documents/' + req.files.doc.name, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  var docLoc = '/files/documents/' + req.files.doc.name;
+  var newDoc = { title: title, date: date, docLoc: docLoc };
+
+  Docs.create(newDoc, function (err) {
+    if (err || !newDoc) {
+      console.log(err);
+      req.flash('error', 'something went wrong');
+      res.redirect('back');
+    } else {
+      req.flash('Success', 'Document upload complete');
+      res.redirect('/info/docs');
+    }
+  });
+});
+
+router.get('/docs/:id', middleware.isAdmin, function (req, res) {
+  Docs.findById(req.params.id, function (err, foundDoc) {
+    if (err) {
+      req.flash('error', 'Something went wrong');
+      res.redirect('back');
+    } else {
+      res.render('info/docs/edit', { foundDoc: foundDoc });
+    }
+  });
+});
+
+router.put('/docs/:id', middleware.isAdmin, function (req, res) {
+  Docs.findByIdAndUpdate(req.params.id, req.body, function (err) {
+    if (err) {
+      req.flash('error', 'something went wrong');
+      res.redirect('back');
+    } else {
+      res.redirect('/info/docs');
+    }
+  });
+});
+
+router.delete('/docs/:id', middleware.isAdmin, function (req, res) {
+  Docs.findByIdAndRemove(req.params.id, function (err, removedDoc) {
+    if (err) {
+      req.flash('error', 'Something went wrong');
+      res.redirect('back');
+    } else {
+      fs.unlink('./public' + removedDoc.docLoc, err => {
+        if (err) {
+          req.flash('error', 'File note deleted, but image removed.');
+          res.redirect('back');
+        }
+      });
+      res.redirect('/info/docs');
+    }
+  });
 });
 
 module.exports = router;
