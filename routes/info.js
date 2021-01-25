67cf43054ea20ad610f7b1467f741dfaa2e9665d
Jordan Webb <jordan@decad3.com>
2021-01-25 14:20:17 -0500

    create separate member document model

--- routes/info.js
+++ routes/info.js
@@ -24,91 +24,91 @@ router.get('/email', function (req, res) {
   res.render('info/email');
 });
 
-// router.get('/docs', function (req, res) {
-//   Docs.find({}, function (err, foundDocs) {
-//     if (err || !foundDocs) {
-//       req.flash('error', 'Item not found');
-//       res.redirect('back');
-//     } else {
-//       res.render('info/docs', { foundDocs: foundDocs });
-//     }
-//   });
-// });
+router.get('/docs', function (req, res) {
+  Docs.find({}, function (err, foundDocs) {
+    if (err || !foundDocs) {
+      req.flash('error', 'Item not found');
+      res.redirect('back');
+    } else {
+      res.render('info/docs', { foundDocs: foundDocs });
+    }
+  });
+});
 
-// router.get('/docs/new', middleware.isAdmin, function (req, res) {
-//   Docs.find({}, function (err, newSk) {
-//     if (err) {
-//       req.flash('error', 'Something went wrong');
-//       res.redirect('back');
-//     } else {
-//       res.render('info/docs/new', { newSk: newSk });
-//     }
-//   });
-// });
+router.get('/docs/new', middleware.isAdmin, function (req, res) {
+  Docs.find({}, function (err, newSk) {
+    if (err) {
+      req.flash('error', 'Something went wrong');
+      res.redirect('back');
+    } else {
+      res.render('info/docs/new', { newSk: newSk });
+    }
+  });
+});
 
-// router.post('/docs', middleware.isAdmin, function (req, res) {
-//   var title = req.body.title;
-//   var date = req.body.date;
+router.post('/docs', middleware.isAdmin, function (req, res) {
+  var title = req.body.title;
+  var date = req.body.date;
 
-//   let doc = req.files.doc;
-//   doc.mv('./public/files/documents/' + req.files.doc.name, function (err) {
-//     if (err) {
-//       console.log(err);
-//     }
-//   });
+  let doc = req.files.doc;
+  doc.mv('./public/files/documents/' + req.files.doc.name, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 
-//   var docLoc = '/files/documents/' + req.files.doc.name;
-//   var newDoc = { title: title, date: date, docLoc: docLoc };
+  var docLoc = '/files/documents/' + req.files.doc.name;
+  var newDoc = { title: title, date: date, docLoc: docLoc };
 
-//   Docs.create(newDoc, function (err) {
-//     if (err || !newDoc) {
-//       console.log(err);
-//       req.flash('error', 'something went wrong');
-//       res.redirect('back');
-//     } else {
-//       req.flash('Success', 'Document upload complete');
-//       res.redirect('/info/docs');
-//     }
-//   });
-// });
+  Docs.create(newDoc, function (err) {
+    if (err || !newDoc) {
+      console.log(err);
+      req.flash('error', 'something went wrong');
+      res.redirect('back');
+    } else {
+      req.flash('Success', 'Document upload complete');
+      res.redirect('/info/docs');
+    }
+  });
+});
 
-// router.get('/docs/:id', middleware.isAdmin, function (req, res) {
-//   Docs.findById(req.params.id, function (err, foundDoc) {
-//     if (err) {
-//       req.flash('error', 'Something went wrong');
-//       res.redirect('back');
-//     } else {
-//       res.render('info/docs/edit', { foundDoc: foundDoc });
-//     }
-//   });
-// });
+router.get('/docs/:id', middleware.isAdmin, function (req, res) {
+  Docs.findById(req.params.id, function (err, foundDoc) {
+    if (err) {
+      req.flash('error', 'Something went wrong');
+      res.redirect('back');
+    } else {
+      res.render('info/docs/edit', { foundDoc: foundDoc });
+    }
+  });
+});
 
-// router.put('/docs/:id', middleware.isAdmin, function (req, res) {
-//   Docs.findByIdAndUpdate(req.params.id, req.body, function (err) {
-//     if (err) {
-//       req.flash('error', 'something went wrong');
-//       res.redirect('back');
-//     } else {
-//       res.redirect('/info/docs');
-//     }
-//   });
-// });
+router.put('/docs/:id', middleware.isAdmin, function (req, res) {
+  Docs.findByIdAndUpdate(req.params.id, req.body, function (err) {
+    if (err) {
+      req.flash('error', 'something went wrong');
+      res.redirect('back');
+    } else {
+      res.redirect('/info/docs');
+    }
+  });
+});
 
-// router.delete('/docs/:id', middleware.isAdmin, function (req, res) {
-//   Docs.findByIdAndRemove(req.params.id, function (err, removedDoc) {
-//     if (err) {
-//       req.flash('error', 'Something went wrong');
-//       res.redirect('back');
-//     } else {
-//       fs.unlink('./public' + removedDoc.docLoc, err => {
-//         if (err) {
-//           req.flash('error', 'File note deleted, but image removed.');
-//           res.redirect('back');
-//         }
-//       });
-//       res.redirect('/info/docs');
-//     }
-//   });
-// });
+router.delete('/docs/:id', middleware.isAdmin, function (req, res) {
+  Docs.findByIdAndRemove(req.params.id, function (err, removedDoc) {
+    if (err) {
+      req.flash('error', 'Something went wrong');
+      res.redirect('back');
+    } else {
+      fs.unlink('./public' + removedDoc.docLoc, err => {
+        if (err) {
+          req.flash('error', 'File note deleted, but image removed.');
+          res.redirect('back');
+        }
+      });
+      res.redirect('/info/docs');
+    }
+  });
+});
 
 module.exports = router;
