5ef728d90777976f7e52996e0e88c51208520851
Jordan Webb <jordan@k4avg.com>
2022-03-24 21:20:04 -0400

    again

--- routes/application.js
+++ routes/application.js
@@ -35,7 +35,7 @@ router.get('/', function (req, res) {
 router.post('/', function (req, res) {
   console.log(req.body)
   console.log(req.body.firstName);
-  if(!req.body.firstName.includes("http" || "info") || !req.body.lastName.includes("http" || "info") || !req.body.callsign.includes("http" || "info") || !req.body.favoriteActivities.includes("http" || "info")){
+  if(!req.body.firstName.includes(".") || !req.body.lastName.includes(".") || !req.body.callsign.includes(".") || !req.body.favoriteActivities.includes("http" || "info")){
     Application.create(req.body, (err, newApplicant) => {
       if(err){
         req.flash('error', err.message);
