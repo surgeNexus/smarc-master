fea8bd5d758ff742ec3f86a272279c744b49306d
Jordan Webb <jordan@k4avg.com>
2022-03-24 20:49:44 -0400

    ignore names with http

--- routes/application.js
+++ routes/application.js
@@ -33,50 +33,52 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', function (req, res) {
-  Application.create(req.body, (err, newApplicant) => {
-    if(err){
-      req.flash('error', err.message);
-      res.redirect('back');
-    } else {
-      const today = new Date();
-      const date = today.toLocaleDateString();
-      newApplicant.lastUpdated = date;
-      newApplicant.save();
-      sendEmail(req.body.email, '[SMARC] Application Received', 
-        newApplicant.firstName + ',\n\n' + '\n\n' +
-        'The Smoky Mountain Amateur Radio Club has received your membership applicaiton and the admins have been notified.\n\n' +
-        'Please ensure that you have paid Dues via one of the available methods.\n\n' +
-        'If you have questions or need help, please reach out to webmaster@w4olb.org\n\n' +
-        '\n\n' +
-        "73 de W4OLB\n"
-      );
-      User.find({}, (err, foundUsers) => {
-        if(err){
-          console.log(err);
-        } else {
-          foundUsers.forEach((u) => {
-            if(u.isAdmin){
-              sendEmail(u.email, '[SMARC] New Member Application',
-                u.firstName + ',\n\n' +
-                'An application has been received for ' + newApplicant.firstName + " " + newApplicant.lastName + ', ' + newApplicant.callsign +  '.\n' +
-                'you can reach them by email at ' + newApplicant.email + ' or by phone at ' + newApplicant.phone + "\n\n" +
-                "This is an automated message from w4olb.org\n"
-              );
-            }
-          });
-        }
-      });
-      if(req.body.smarcOnline === true){
-        req.flash('success', "Your application has been submitted. \n\n Please allow time for your membership fee to be processed and your online account to be activated.")
-        res.render('application/appRegister', {
-          info: newApplicant
-        });
+  if(!req.body.firstName.includes("http") || !req.body.firstName.includes("http")){
+    Application.create(req.body, (err, newApplicant) => {
+      if(err){
+        req.flash('error', err.message);
+        res.redirect('back');
       } else {
-        req.flash('success', "Thanks! Your applicaiton has been submitted!")
-        res.redirect('/');
+        const today = new Date();
+        const date = today.toLocaleDateString();
+        newApplicant.lastUpdated = date;
+        newApplicant.save();
+        sendEmail(req.body.email, '[SMARC] Application Received', 
+          newApplicant.firstName + ',\n\n' + '\n\n' +
+          'The Smoky Mountain Amateur Radio Club has received your membership applicaiton and the admins have been notified.\n\n' +
+          'Please ensure that you have paid Dues via one of the available methods.\n\n' +
+          'If you have questions or need help, please reach out to webmaster@w4olb.org\n\n' +
+          '\n\n' +
+          "73 de W4OLB\n"
+        );
+        User.find({}, (err, foundUsers) => {
+          if(err){
+            console.log(err);
+          } else {
+            foundUsers.forEach((u) => {
+              if(u.isAdmin){
+                sendEmail(u.email, '[SMARC] New Member Application',
+                  u.firstName + ',\n\n' +
+                  'An application has been received for ' + newApplicant.firstName + " " + newApplicant.lastName + ', ' + newApplicant.callsign +  '.\n' +
+                  'you can reach them by email at ' + newApplicant.email + ' or by phone at ' + newApplicant.phone + "\n\n" +
+                  "This is an automated message from w4olb.org\n"
+                );
+              }
+            });
+          }
+        });
+        if(req.body.smarcOnline === true){
+          req.flash('success', "Your application has been submitted. \n\n Please allow time for your membership fee to be processed and your online account to be activated.")
+          res.render('application/appRegister', {
+            info: newApplicant
+          });
+        } else {
+          req.flash('success', "Thanks! Your applicaiton has been submitted!")
+          res.redirect('/');
+        }
       }
-    }
-  });
+    });
+  }
 });
 
 router.get('/roster', middleware.isAdmin, (req, res) => {
